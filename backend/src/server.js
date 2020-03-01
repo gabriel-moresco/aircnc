f115688c23412085d83fd35562e3543ae170ca58
Gabriel Moresco <gabriel.moresco25@gmail.com>
2020-03-01 00:03:26 -0300

    Alterações do backend para comportar websocket

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,17 +3,39 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+const connectedUsers = {};
 
 mongoose.connect(
 	'mongodb+srv://test:test@test-jdytj.mongodb.net/test?retryWrites=true&w=majority',
 	{ useNewUrlParser: true, useUnifiedTopology: true }
 );
 
+io.on('connection', socket => {
+	console.log(socket.handshake.query);
+	console.log('Usuário conectado', socket.id);
+
+	const { user_id } = socket.handshake.query;
+
+	connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+	req.io = io;
+	req.connectedUsers = connectedUsers;
+
+	return next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
